2688ef2ac7ff890a0c9c47f393d68bab52775651
Marvin-Kabugi <kabugijeff@gmail.com>
2023-06-06 22:24:35 +0300

    completed assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,30 @@
 class MySet:
+    def __init__(self, list=[]) -> None:
+        self.dictionary = {}
+        for value in list:
+            self.dictionary[value] = True
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        if not self.has(value):
+            self.dictionary[value] = True
+        return self
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+        return self
+    
+    def __str__(self) -> str:
+        set_list = []
+        for key, value in self.dictionary.items():
+            set_list.append(str(key))
+        return f'MySet: {{{",".join(set_list)}}}'
